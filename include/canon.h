a34ba0d23e1d4e67e9f014de8dd76d4b423bc31b
barsbold-coding <barsbold.coding@gmail.com>
2026-05-23 16:00:16 +0800

    Add basic blocks and trace scheduling to canon module

--- include/canon.h
+++ include/canon.h
@@ -8,6 +8,18 @@ typedef struct stmt_list_t {
   struct stmt_list_t *next;
 } stmt_list_t;
 
+typedef struct block_t {
+  stmt_list_t *stmts;
+  struct block_t *next;
+} block_t;
+
+typedef struct {
+  block_t *blocks;
+  label_t  done;
+} block_res_t;
+
 stmt_list_t *linearize(tree_stmt_t *s);
+block_res_t *basic_blocks(stmt_list_t *stmts);
+stmt_list_t *trace_schedule(block_res_t *res);
 
 #endif
--- src/canon/canon.c
+++ src/canon/canon.c
@@ -1,6 +1,7 @@
 #include "canon.h"
 #include "tree.h"
 #include "temp.h"
+#include "symtab.h"
 #include <stdlib.h>
 
 typedef struct {
@@ -102,4 +103,115 @@ static exp_res_t *do_exp(tree_expr_t *e) {
 
 stmt_list_t *linearize(tree_stmt_t *s) {
   return linear(do_stm(s), NULL);
+}
+
+block_res_t *basic_blocks(stmt_list_t *stmts) {
+  label_t done = label_new();
+  if (!stmts || stmts->stmt->kind != TREE_LABEL) {
+    stmt_list_t *sl = malloc(sizeof(stmt_list_t));
+    sl->stmt = tree_label(label_new());
+    sl->next = stmts;
+    stmts = sl;
+  }
+
+  block_t *blocks = NULL, *blast = NULL;
+  stmt_list_t *cur = stmts;
+
+  while (cur) {
+    stmt_list_t *block_head = cur;
+    stmt_list_t *block_tail = cur;
+    cur = cur->next;
+
+    while (cur) {
+      if (cur->stmt->kind == TREE_JUMP || cur->stmt->kind == TREE_CJUMP) {
+        block_tail->next = cur;
+        block_tail = cur;
+        cur = cur->next;
+        block_tail->next = NULL;
+        break;
+      } else if (cur->stmt->kind == TREE_LABEL) {
+        stmt_list_t *jmp = malloc(sizeof(stmt_list_t));
+        label_t *labels = malloc(sizeof(label_t));
+        labels[0] = cur->stmt->label;
+        jmp->stmt = tree_jump(tree_name(labels[0]), labels, 1);
+        jmp->next = NULL;
+        block_tail->next = jmp;
+        block_tail = jmp;
+        break;
+      } else {
+        block_tail = cur;
+        cur = cur->next;
+      }
+    }
+
+    if (!cur && block_tail->stmt->kind != TREE_JUMP && block_tail->stmt->kind != TREE_CJUMP) {
+      stmt_list_t *jmp = malloc(sizeof(stmt_list_t));
+      label_t *labels = malloc(sizeof(label_t));
+      labels[0] = done;
+      jmp->stmt = tree_jump(tree_name(done), labels, 1);
+      jmp->next = NULL;
+      block_tail->next = jmp;
+      block_tail = jmp;
+    }
+
+    block_t *b = malloc(sizeof(block_t));
+    b->stmts = block_head;
+    b->next  = NULL;
+    if (blast) blast = blast->next = b;
+    else       blocks = blast = b;
+  }
+
+  block_res_t *res = malloc(sizeof(block_res_t));
+  res->blocks = blocks;
+  res->done   = done;
+  return res;
+}
+
+stmt_list_t *trace_schedule(block_res_t *res) {
+  symtab_t *tab = symtab_new(64);
+  symtab_enter_scope(tab);
+  for (block_t *b = res->blocks; b; b = b->next) {
+    symtab_insert(tab, label_name(b->stmts->stmt->label), b);
+  }
+
+  stmt_list_t *result = NULL, *rlast = NULL;
+
+  for (block_t *b = res->blocks; b; b = b->next) {
+    if (symtab_lookup(tab, label_name(b->stmts->stmt->label)) == NULL)
+      continue;
+
+    block_t *trace = b;
+    while (trace) {
+      symtab_insert(tab, label_name(trace->stmts->stmt->label), NULL);
+
+      stmt_list_t *tail = NULL;
+      for (stmt_list_t *sl = trace->stmts; sl; sl = sl->next) {
+        stmt_list_t *node = malloc(sizeof(stmt_list_t));
+        node->stmt = sl->stmt;
+        node->next = NULL;
+        if (rlast) rlast = rlast->next = node;
+        else       result = rlast = node;
+        tail = node;
+      }
+
+      block_t *next = NULL;
+      tree_stmt_t *last = tail->stmt;
+      if (last->kind == TREE_JUMP) {
+        next = symtab_lookup(tab, label_name(last->jump_.dests[0]));
+      } else if (last->kind == TREE_CJUMP) {
+        next = symtab_lookup(tab, label_name(last->cjump.false_));
+        if (!next) next = symtab_lookup(tab, label_name(last->cjump.true_));
+      }
+      trace = next;
+    }
+  }
+
+  stmt_list_t *donenode = malloc(sizeof(stmt_list_t));
+  donenode->stmt = tree_label(res->done);
+  donenode->next = NULL;
+  if (rlast) rlast->next = donenode;
+  else result = donenode;
+
+  symtab_exit_scope(tab);
+  return result;
 }
